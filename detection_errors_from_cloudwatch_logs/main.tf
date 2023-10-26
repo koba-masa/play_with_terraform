9aaa010a94d0d8fa8e0441f7014250752bddad4f
koba-masa <kobayashi_mshr@koba-masa.com>
2023-10-26 21:53:43 +0900

    feat: サブスクリプションフィルターを追加する

--- detection_errors_from_cloudwatch_logs/main.tf
+++ detection_errors_from_cloudwatch_logs/main.tf
@@ -1,5 +1,18 @@
 locals {
-  tags = merge(var.common_tags, { Environment = var.environment })
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+  tags       = merge(var.common_tags, { Environment = var.environment })
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+module "detection_errors_from_cloudwatch_logs" {
+  source                  = "./modules/cloudwatch"
+  account_id              = local.account_id
+  region                  = local.region
+  log_group_name          = "/play_with_terraform/detection_errors_from_cloudwatch_logs"
+  notification_lambda_arn = module.common.notification_lambda_arn
 }
 
 module "common" {
---
+++ detection_errors_from_cloudwatch_logs/modules/common/output.tf
@@ -0,0 +1,3 @@
+output "notification_lambda_arn" {
+  value = aws_lambda_function.notification_lambda.arn
+}
