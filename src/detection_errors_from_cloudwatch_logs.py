2c089bb572d34149b3698dd1dbf4f43b370677fd
koba-masa <kobayashi_mshr@koba-masa.com>
2023-10-27 23:01:07 +0900

    feat: CloudWatchログをSNSにて送信する

--- src/detection_errors_from_cloudwatch_logs.py
+++ src/detection_errors_from_cloudwatch_logs.py
@@ -3,7 +3,8 @@ import base64
 import gzip
 import logging
 import sys
-
+import boto3
+import os
 
 def lambda_handler(event, context):
     return ErrorNotification(event, context).execute()
@@ -16,10 +17,13 @@ class ErrorNotification():
         self.event = event
         self.context = context
         self.decoded_data = self.__decode_awslogs_data()
+        self.sns_client = None
 
 
-    def execute(self):
+    def execute(self) -> dict:
         self.logger.info(self.decoded_data['logEvents'][0])
+        message = self.decoded_data['logEvents'][0]['message']
+        self.__send_email(message)
 
         return {
             'statusCode': 200,
@@ -27,7 +31,7 @@ class ErrorNotification():
         }
 
 
-    def __initialize_logger(self):
+    def __initialize_logger(self) -> logging.Logger:
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
         #handler = logging.StreamHandler(sys.stdout)
@@ -36,7 +40,7 @@ class ErrorNotification():
         return logger
 
 
-    def __decode_awslogs_data(self):
+    def __decode_awslogs_data(self) -> dict:
         json_data = None
         encoded_and_gzip_compressed_data = self.event['awslogs']['data']
         gzip_compressed_data = base64.b64decode(encoded_and_gzip_compressed_data)
@@ -44,3 +48,18 @@ class ErrorNotification():
         json_data = json.loads(text_data)
 
         return json_data
+
+
+    def __sns_client(self) -> boto3.client:
+        if self.sns_client is None:
+            self.sns_client = boto3.client('sns')
+
+        return self.sns_client
+
+
+    def __send_email(self, message: str) -> None:
+        self.__sns_client().publish(
+            TopicArn = os.environ['SNS_TOPIC_ARN'],
+            Subject = 'Transfer CloudWatch Logs to SNS Email',
+            Message = message,
+        )
