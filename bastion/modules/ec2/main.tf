d8faef54c53ced3daa6d34e4d949ae2c7a3b2f4b
koba-masa <kobayashi_mshr@koba-masa.com>
2023-11-16 12:33:03 +0900

    feat: ElasticIPを構築する

--- bastion/modules/ec2/main.tf
+++ bastion/modules/ec2/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux_2023.id
   instance_type = "t2.micro"
 
-  subnet_id       = var.subnet_id
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_groups
 
   instance_initiated_shutdown_behavior = "stop"
@@ -21,6 +21,18 @@ resource "aws_instance" "bastion" {
   }
 
   key_name = var.key_name
+
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+    Identifier  = "bastion"
+    Name        = "${var.project}-${var.environment_short}-bastion"
+  }
+}
+
+resource "aws_eip" "bastion_ip_address" {
+  instance = aws_instance.bastion.id
+  domain   = "vpc"
 
   tags = {
     Project     = var.project
