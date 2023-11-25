94cf7be54ebd784d59ebfc9cf01caaec68e4dd4e
koba-masa <kobayashi_mshr@koba-masa.com>
2023-11-25 23:00:14 +0900

    refactor: バースト可能なRDSインスタンスクラスを使用できるようにする

--- aurora_multi_az/modules/vpc/main.tf
+++ aurora_multi_az/modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   cidr_block                       = var.cidr_block
   assign_generated_ipv6_cidr_block = false
 
-  instance_tenancy = "dedicated"
+  instance_tenancy = "default"
 
   enable_dns_support                   = true
   enable_dns_hostnames                 = false
