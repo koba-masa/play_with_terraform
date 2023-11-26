31be0ef4f3d14483ecf40e3167838e038cc43126
koba-masa <kobayashi_mshr@koba-masa.com>
2023-11-26 10:57:17 +0900

    feat: remote_stateを使用するための環境を構築する

---
+++ remote_state/output.tf
@@ -0,0 +1,6 @@
+output "production" {
+  value = {
+    vpc_id            = data.terraform_remote_state.network.outputs.network.vpc_id
+    public_subnet_ids = data.terraform_remote_state.network.outputs.network.public_subnet_ids
+  }
+}
