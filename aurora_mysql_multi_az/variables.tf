55b8ff5a8afc3646b312225c1cc414f9598f675e
koba-masa <kobayashi_mshr@koba-masa.com>
2023-11-26 12:19:12 +0900

    feat: マルチAZ対応のRDSを構築するための雛形を作成する

---
+++ aurora_mysql_multi_az/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
