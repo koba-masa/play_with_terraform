cc96167187652e288f1b964bdbcb9621e3f07c11
koba-masa <kobayashi_mshr@koba-masa.com>
2023-11-26 12:35:23 +0900

    feat: 踏み台サーバーのセキュリティーグループを外部から利用できるようにする

--- remote_state/output.tf
+++ remote_state/output.tf
@@ -2,5 +2,7 @@ output "export" {
   value = {
     vpc_id            = data.terraform_remote_state.network.outputs.network.vpc_id
     public_subnet_ids = data.terraform_remote_state.network.outputs.network.public_subnet_ids
+
+    bastion_security_group_id = data.terraform_remote_state.bastion.outputs.bastion.bastion_security_group_id
   }
 }
