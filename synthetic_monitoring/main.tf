223597daa4bceeb26c360e7d831bcec2824ab6c7
koba-masa <kobayashi_mshr@koba-masa.com>
2024-01-06 20:46:43 +0900

    feat: Syntheticのログを格納するためのバケットを作成する

--- synthetic_monitoring/main.tf
+++ synthetic_monitoring/main.tf
@@ -1,3 +1,9 @@
+module "bucket" {
+  source      = "./modules/s3"
+  project     = var.project
+  environment = var.environment
+}
+
 module "role" {
   source      = "./modules/role"
   project     = var.project
